0595986ccb858f4d2f6c799419a4b9ce432f6662
James Lippitt <james.lippitt@gmail.com>
2024-03-30 09:24:47 +0000

    Load PIF data into an array

---
+++ cli/src/main.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    pif_data_path: String,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    config_path: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let config: Config = {
+        let config_data = fs::read_to_string(args.config_path)?;
+        toml::from_str(&config_data)?
+    };
+
+    let pif_data = {
+        let pif_data_path = shellexpand::full(&config.pif_data_path)?;
+        fs::read(pif_data_path.as_ref())?
+    };
+
+    println!("{:?}", pif_data);
+
+    Ok(())
+}
