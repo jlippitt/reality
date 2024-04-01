17c9c0da0a3b7f3a2dacaa7b6bd8f28539711ef3
James Lippitt <james.lippitt@gmail.com>
2024-04-01 16:41:32 +0100

    Populate PI address registers

--- system/src/peripheral.rs
+++ system/src/peripheral.rs
@@ -1,24 +1,42 @@
 use crate::cpu::Size;
 use crate::memory::WriteMask;
-use tracing::warn;
+use tracing::{trace, warn};
 
 pub struct PeripheralInterface {
-    //
+    dram_addr: u32,
+    cart_addr: u32,
 }
 
 impl PeripheralInterface {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            dram_addr: 0,
+            cart_addr: 0,
+        }
     }
 
     pub fn read<T: Size>(&self, address: u32) -> T {
-        todo!("PI Register Read: {:08X}", address);
+        T::from_u32(match address >> 2 {
+            4 => {
+                // TODO: DMA status
+                0
+            }
+            _ => todo!("PI Register Read: {:08X}", address),
+        })
     }
 
     pub fn write<T: Size>(&mut self, address: u32, value: T) {
         let mask = WriteMask::new(address, value);
 
         match address >> 2 {
+            0 => {
+                mask.write(&mut self.dram_addr);
+                trace!("PI_DRAM_ADDR: {:08X}", self.dram_addr);
+            }
+            1 => {
+                mask.write(&mut self.cart_addr);
+                trace!("PI_CART_ADDR: {:08X}", self.cart_addr);
+            }
             4 => {
                 let raw = mask.raw();
 
