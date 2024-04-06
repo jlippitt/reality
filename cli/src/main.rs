2e801142bce4852d551f8ddd39a898c253fc976c
James Lippitt <james.lippitt@gmail.com>
2024-04-06 13:08:26 +0100

    For now remove the config file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use serde::Deserialize;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
@@ -13,33 +12,20 @@ use winit::window::WindowBuilder;
 
 mod log;
 
-#[derive(Deserialize, Debug)]
-struct Config {
-    pif_data_path: String,
-}
-
 #[derive(Parser, Debug)]
 struct Args {
     rom_path: PathBuf,
 
     #[arg(short, long)]
-    config_path: PathBuf,
+    pif_data_path: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let config: Config = {
-        let config_data = fs::read_to_string(args.config_path)?;
-        toml::from_str(&config_data)?
-    };
-
-    let pif_data = {
-        let pif_data_path = shellexpand::full(&config.pif_data_path)?;
-        fs::read(pif_data_path.as_ref())?
-    };
-
     let rom_data = fs::read(args.rom_path)?;
+
+    let pif_data = fs::read(args.pif_data_path)?;
 
     let _guard = log::init()?;
 
