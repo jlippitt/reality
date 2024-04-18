25e983982fe956a260841707a811873ef7d5f968
James Lippitt <james.lippitt@gmail.com>
2024-04-18 12:49:05 +0100

    Ignore some additional sync commands

--- system/src/rdp/core/sync.rs
+++ system/src/rdp/core/sync.rs
@@ -2,8 +2,20 @@ use super::{Bus, Core};
 use bitfield_struct::bitfield;
 use tracing::trace;
 
+pub fn sync_load(_core: &mut Core, _bus: Bus, word: u64) {
+    let cmd = SyncLoad::from(word);
+    trace!("{:?}", cmd);
+    // TODO
+}
+
 pub fn sync_pipe(_core: &mut Core, _bus: Bus, word: u64) {
     let cmd = SyncPipe::from(word);
+    trace!("{:?}", cmd);
+    // TODO
+}
+
+pub fn sync_tile(_core: &mut Core, _bus: Bus, word: u64) {
+    let cmd = SyncTile::from(word);
     trace!("{:?}", cmd);
     // TODO
 }
@@ -15,7 +27,17 @@ pub fn sync_full(_core: &mut Core, _bus: Bus, word: u64) {
 }
 
 #[bitfield(u64)]
+struct SyncLoad {
+    __: u64,
+}
+
+#[bitfield(u64)]
 struct SyncPipe {
+    __: u64,
+}
+
+#[bitfield(u64)]
+struct SyncTile {
     __: u64,
 }
 
